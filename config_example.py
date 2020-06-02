64c83605ca0f1a212de4703787804ebe62a9a470
Donny Son <donny@bagstation.io>
2020-06-02 23:42:27 +0900

    Update config.py

--- config_example.py
+++ config_example.py
@@ -3,6 +3,8 @@ import os
 from dataclasses import dataclass
 
 
+
+DEBUG = True
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 LOG_DIR = ROOT_DIR + '/' + 'storage' + '/' + 'log'
 
@@ -10,9 +12,10 @@ LOG_DIR = ROOT_DIR + '/' + 'storage' + '/' + 'log'
 # Slack
 @dataclass
 class SlackConfig:
-    CHANNEL = 'stats-career-yonsei'
+    CHANNEL = {}
+    CHANNEL['career'] = 'stats-career-yonsei' if not DEBUG else 'debug'
+    CHANNEL['news'] = 'stats-grad-billboard' if not DEBUG else 'debug'
     SLACK_API_TOKEN = ""
-
 
 if __name__ == '__main__':
     print(ROOT_DIR)
